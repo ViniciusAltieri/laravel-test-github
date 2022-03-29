d764b42bdc497f6603cfc41d78fd4185dce41010
vinicius <vikeoch@hotmail.com>
2022-03-29 10:32:16 -0300

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class UserController extends Controller
+{
+    public function registro() {
+        echo ("Usando a função de dentro do controlador!");
+    }
+}
